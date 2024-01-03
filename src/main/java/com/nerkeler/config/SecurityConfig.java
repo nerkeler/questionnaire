98efe5fc200f9552aae256878b212ffc25f16d87
nerkeler <2739038007@qq.com>
2024-01-03 17:37:09 +0800

    问卷后端，创建文件

---
+++ src/main/java/com/nerkeler/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.nerkeler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{bcrypt}$2a$10$4RyfuMnHbMhWs3X5DcX5A.BDh4X1pW5cZ7Ft0v3N2waZV8mKpZ8C2") // 使用bcrypt加密的密码，示例密码为"password"
+                .roles("admin");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/home").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/home")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .permitAll();
+    }
+}
